06276828c83bf23a7fda8767abba8584a932fc55
filip123e <44704844+filip123e@users.noreply.github.com>
2025-06-05 12:20:26 +0200

    Update imenik.h

--- imenik.h
+++ imenik.h
@@ -2,7 +2,9 @@
 #define IMENIK_H
 
 #include <stddef.h>
+#include <stdio.h>
 
+//3,4,5,15
 #define MAX_IME 50
 #define MAX_PREZIME 50
 #define MAX_BROJ 20
@@ -10,13 +12,14 @@
 #define DATOTEKA "imenik.txt"
 
 typedef struct {
-    int id;
-    char ime[MAX_IME];
-    char prezime[MAX_PREZIME];
-    char broj[MAX_BROJ];
-    char email[MAX_EMAIL];
+    int id; //2
+    char ime[MAX_IME];  //15
+    char prezime[MAX_PREZIME]; //15
+    char broj[MAX_BROJ]; //15
+    char email[MAX_EMAIL]; //15
 } Kontakt;
 
+//4
 typedef enum {
     IZLAZ = 0,
     UNOS,
@@ -27,22 +30,24 @@ typedef enum {
     PRETRAZI,
     ARHIVIRAJ
 } IzbornikOpcije;
-
+//8
 extern Kontakt* kontakti;
 extern size_t brojKontakata;
-
+//13
 void ucitajIzDatoteke(void);
 void spremiUDatoteku(void);
-void unosKontakta(void);
-void pregledKontakata(void);
-void azurirajKontakt(void);
-void obrisiKontakt(void);
-void sortirajKontakte(void);
-void pretraziKontakt(void);
-void arhivirajDatoteku(void);
-void ocistiMemoriju(void);
+void unosKontakta(void);//1
+void pregledKontakata(void);//1
+void azurirajKontakt(void);//1
+void obrisiKontakt(void);//1
+void sortirajKontakte(void);//23
+void pretraziKontakt(void);//24
+void arhivirajDatoteku(void);//21
+void ocistiMemoriju(void);//18
 
 int usporedbaKontakta(const void*, const void*);
-void pauziraj(void);
+//9
+void pauziraj(void); 
+
 
 #endif
