5ffc510c31fd1d3edd0ca55d750fec4485873579
filip123e <44704844+filip123e@users.noreply.github.com>
2025-06-30 21:10:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 
 int main(void) {
     int opcija;
-    ucitajIzDatoteke();//19
-    //10
+    ucitajIzDatoteke();   
+
     do {
         printf("\n--- Telefonski imenik ---\n");
         printf("1. Unos kontakta\n");
         printf("2. Pregled kontakata\n");
-        printf("3. Azuriraj kontakt\n");
-        printf("4. Obrisi kontakt\n");
+        printf("3. Ažuriraj kontakt\n");
+        printf("4. Obriši kontakt\n");
         printf("5. Sortiraj kontakte\n");
-        printf("6. Pretrazi kontakt\n");
+        printf("6. Pretraži kontakt\n");
         printf("7. Arhiviraj imenik\n");
         printf("0. Izlaz\n");
         printf("Odabir: ");
@@ -23,16 +23,17 @@ int main(void) {
             opcija = -1;
             continue;
         }
-        while (getchar() != '\n');
+        while (getchar() != '\n');       
+
         switch (opcija) {
-        case UNOS: unosKontakta(); break;
-        case PREGLED: pregledKontakata(); break;
-        case AZURIRAJ: azurirajKontakt(); break;
-        case OBRISI: obrisiKontakt(); break;
+        case UNOS:     unosKontakta();     break;
+        case PREGLED:  pregledKontakata(); break;
+        case AZURIRAJ: azurirajKontakt();  break;
+        case OBRISI:   obrisiKontakt();    break;
         case SORTIRAJ: sortirajKontakte(); break;
-        case PRETRAZI: pretraziKontakt(); break;
+        case PRETRAZI: pretraziKontakt();  break;
         case ARHIVIRAJ: arhivirajDatoteku(); break;
-        case IZLAZ: ocistiMemoriju(); break;
+        case IZLAZ:    ocistiMemoriju();   break;
         default:
             printf("Nepoznata opcija!\n");
             pauziraj();
